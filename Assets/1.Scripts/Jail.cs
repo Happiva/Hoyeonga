e0e34b22e24b46cf920ed66d8633d2f5a007cb9d
Happiva <kiwhhb1016@gmail.com>
2021-05-19 22:16:57 +0900

    철창, 먼지 애니메이션 추가 및 철창 해방 일부  구현

---
+++ Assets/1.Scripts/Jail.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jail : MonoBehaviour
+{
+    private BoxCollider2D collider;
+    private Rigidbody2D rigid;
+    private Animator ani;
+    public GameObject dust;
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        rigid = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ani.SetBool("Broke", true);
+            rigid.constraints = RigidbodyConstraints2D.None;
+            rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {            
+            //rigid.constraints = RigidbodyConstraints2D.FreezePositionX;
+            //rigid.constraints = RigidbodyConstraints2D.FreezePositionY;
+            dust.GetComponent<Animator>().SetTrigger("Play");
+        }
+    }
+}
